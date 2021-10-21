8b3380831f80ceae0c65a60f5a5d77fcd0bd681f
MiloDC <cooper.milo@outlook.com>
2021-10-20 22:44:19 -0700

    Renamed Global to Core.

--- Core.fs
+++ Core.fs
@@ -1,20 +1,20 @@
 ﻿[<AutoOpen>]
-module net.miloonline.MuSvc.Global
+module net.miloonline.MuSvc.Core
 
-open System
+open System.Text
 open System.Runtime.InteropServices
-
-type MuSvcResult =
-    | Bytes of byte array
-    | Text of string
-    | Error of string
 
 [<RequireQualifiedAccess>]
 module private Native =
     [<DllImport ("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)>]
     extern int memcmp (byte[] bytes1, byte[] bytes2, int64 count)
 
-let internal MsgTerminatorBytes = "\r\n" |> Text.Encoding.UTF8.GetBytes
+type MuSvcResult =
+    | Bytes of byte array
+    | Text of string
+    | Error of string
+
+let internal MsgTerminatorBytes = "\r\n" |> Encoding.UTF8.GetBytes
 
 let internal bytesMatch (bytes1 : byte array) (bytes2 : byte array) =
     (bytes1.LongLength = bytes2.LongLength)
@@ -22,5 +22,5 @@ let internal bytesMatch (bytes1 : byte array) (bytes2 : byte array) =
 
 [<RequireQualifiedAccess>]
 module internal Command =
-    let ClientCount = "/cc" |> Text.Encoding.UTF8.GetBytes
-    let Quit = "/q" |> Text.Encoding.UTF8.GetBytes
+    let ClientCount = "/cc" |> Encoding.UTF8.GetBytes
+    let Quit = "/q" |> Encoding.UTF8.GetBytes
